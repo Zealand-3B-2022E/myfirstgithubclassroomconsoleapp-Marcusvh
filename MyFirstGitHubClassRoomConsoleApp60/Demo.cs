2ba1b404ea206048f95e2154bcd986802474918d
Marcus Vibe Hjulgaard <marc3277@edu.easj.dk>
2022-09-05 12:44:25 +0200

    added class with properties

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    internal class Demo
+    {
+        private int _id;
+        private int _number;
+        private string _name;
+        private string _description;
+
+        public Demo()
+        {
+            _id = 0;
+            _number = 0;
+            _name = "";
+            _description = "";
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public int Number
+        {
+            get { return _number; }
+            set { _number = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                _description = value;
+            }
+        }
+    }
+}
